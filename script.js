1e57105ad7829f992a26087338158f5eea6e6d0d
Saurabhchaddha6 <92564392+Saurabhchaddha6@users.noreply.github.com>
2023-08-06 02:12:39 +0530

    quotes display success

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-fetch('https://api.themotivate365.com/stoic-quote')
-    .then(res => res.json())
-    .then(data => console.log(data.quote));
+
+// const quote_text = fetch('https://api.themotivate365.com/stoic-quote')
+//     .then(res => res.json())
+//     .then(data => console.log(data.quote));
+
+
+// console.log(change);
+
+const api_url = "https://api.themotivate365.com/stoic-quote"
+
+async function api_quote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+
+    const change = document.getElementById('quote');
+    change.innerHTML = data.quote;
+}
+api_quote(api_url);
+
